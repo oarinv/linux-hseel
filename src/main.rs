a0f24530d8e647de74e300ee060f6ca8947c6f9b
oarinv <zenp.me@yandex.com>
2023-07-02 09:27:54 +0800

    🥵

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use std::process::Command;
+extern crate ini;
+use ini::Ini;
+
+fn main() {
+    // 读取配置
+    let conf = Ini::load_from_file("config.ini").unwrap();
+
+    let section = conf.section(Some("config")).unwrap();
+    let tommy = section.get("command").unwrap();
+
+    println!("{:?}", tommy);
+    let output = Command::new("bash")
+        .arg("-c")
+        .arg(tommy)
+        .output()
+        .expect("failed to execute process");
+    let out = String::from_utf8(output.stdout).unwrap();
+    println!("{}", out);
+}
